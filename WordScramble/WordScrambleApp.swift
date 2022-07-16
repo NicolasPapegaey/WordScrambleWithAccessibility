e6b4c8f007b2417e7c117a1a2d850cf46ba45c5c
Nicolas Papegaey <nicolas.papegaey@gmail.com>
2022-07-16 12:16:42 +0200

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Nicolas Papegaey on 29/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
